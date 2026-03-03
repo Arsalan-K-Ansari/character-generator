f694106551160591c13d03d6be8d26bb62436e56
Arsalan Ansari <shani@Mac.fios-router.home>
2026-03-03 14:15:26 -0500

    Fix image routes require path

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -5,13 +5,13 @@ const chatgptRoutes = require('./chatgpt-routes');
 const classRoutes = require('./class-routes');
 const raceRoutes = require('./race-routes');
 const charRoutes = require('./character-routes');
-const openAiRoute = require('./openai-image-routes');
+const imageRoutes = require("./image-routes");
 
 router.use('/users', userRoutes);
 router.use('/chatgpt', chatgptRoutes);
 router.use('/classes', classRoutes);
 router.use('/races', raceRoutes);
 router.use('/characters', charRoutes);
-router.use('/images', openAiRoute);
+router.use("/images", imageRoutes);
 
 module.exports = router;
